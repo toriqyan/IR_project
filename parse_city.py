36f4ccdbb71c0c570d9080c879017b8493f49981
Tori Yan <qiaoiyan@cs.washington.edu>
2017-11-30 14:35:57 -0500

    adding output file

--- parse_city.py
+++ parse_city.py
@@ -1,14 +1,17 @@
 import json
 
 city_name = 'San Francisco'
+city_initial = 'SF'
 
 json_data = open('business.json')
 restaurants = json.loads(json_data)
 json_data.close()
+print(len(restaurants))
 
 json_data = open('review.json')
 reviews = json.loads(json_data)
 json_data.close()
+print(len(reviews))
 
 target_restaurants = []
 target_reviews = []
@@ -19,3 +22,9 @@ for restaurant in restaurants:
 		for review in reviews:
 			if review.business_id == restaurant.business_id:
 				target_reviews.append(review)
+
+with open('business_'+city_initial+'.json', 'w+') as business_out:
+	json.dumps(target_restaurants, business_out)
+
+with open('review_'+city_initial+'.json', 'w+') as reviews_out:
+	json.dumps(target_reviews, reviews_out)
